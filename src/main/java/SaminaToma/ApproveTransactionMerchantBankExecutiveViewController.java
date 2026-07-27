ee1cbca09d86ba008302a942eadc5bccf6a1ed43
Samina Sultana Toma <2310739@iub.edu.bd>
2026-07-28 02:57:31 +0600

    Added ApproveTransactionMBE,ComplianceMBE,RecordsMBE,PortfolioMBE,ReportMBE

---
+++ src/main/java/SaminaToma/ApproveTransactionMerchantBankExecutiveViewController.java
@@ -0,0 +1,38 @@
+package SaminaToma;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ApproveTransactionMerchantBankExecutiveViewController
+{
+    @javafx.fxml.FXML
+    private TableColumn transactionIDColUser7;
+    @javafx.fxml.FXML
+    private TableColumn amountColUser7;
+    @javafx.fxml.FXML
+    private TableColumn accNumColUser7;
+    @javafx.fxml.FXML
+    private TextField approvalRemarksTF;
+    @javafx.fxml.FXML
+    private TableView approvetransactionTV;
+    @javafx.fxml.FXML
+    private TableColumn transactionDateColUser7;
+    @javafx.fxml.FXML
+    private TableColumn transactionTypeColUser7;
+    @javafx.fxml.FXML
+    private TableColumn statusColUser7;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleApproveForApproveTransactionOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleBackForApproveTransactionOA(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/SaminaToma/ComplianceMerchantBankExecutiveViewController.java
@@ -0,0 +1,41 @@
+package SaminaToma;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class ComplianceMerchantBankExecutiveViewController
+{
+    @javafx.fxml.FXML
+    private TextField accNoTF;
+    @javafx.fxml.FXML
+    private TableColumn compIDColUser7;
+    @javafx.fxml.FXML
+    private TextField compIDTF;
+    @javafx.fxml.FXML
+    private ComboBox statusCB;
+    @javafx.fxml.FXML
+    private TableColumn accNoColUser7;
+    @javafx.fxml.FXML
+    private TableView complianceTV;
+    @javafx.fxml.FXML
+    private TableColumn statusColUser7;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleClearForComplianceOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleValidateForComplianceOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleVerifyForComplianceOA(ActionEvent actionEvent) {
+    }
+}
