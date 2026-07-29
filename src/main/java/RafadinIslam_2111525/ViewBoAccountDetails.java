a90989deaa5fb2cb38ca3860e3cf957f0690e3f1
Laralarala <rafadinislamzidan@gmail.com>
2026-07-29 12:04:54 +0600

    first fxml file and java class commit

---
+++ src/main/java/RafadinIslam_2111525/ViewBoAccountDetails.java
@@ -0,0 +1,29 @@
+package RafadinIslam_2111525;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ViewBoAccountDetails
+{
+    @javafx.fxml.FXML
+    private TableColumn accountTypeTableCol;
+    @javafx.fxml.FXML
+    private TableColumn depositoryParticipantsTableCol;
+    @javafx.fxml.FXML
+    private TableColumn boIdTableCol;
+    @javafx.fxml.FXML
+    private TableView boAccountDetailsTableView;
+    @javafx.fxml.FXML
+    private TableColumn bankNameTableCol;
+    @javafx.fxml.FXML
+    private TableColumn accountNameTableCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void viewBoAccountDetailsOnActionButton(ActionEvent actionEvent) {
+    }
+}
