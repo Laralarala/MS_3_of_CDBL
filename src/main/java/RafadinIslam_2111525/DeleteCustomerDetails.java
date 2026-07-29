86bd9b4be5fbcc85e599faf2c674f14644ec0704
Laralarala <rafadinislamzidan@gmail.com>
2026-07-29 18:53:09 +0600

    commiting delete customer data fxml and java

---
+++ src/main/java/RafadinIslam_2111525/DeleteCustomerDetails.java
@@ -0,0 +1,36 @@
+package RafadinIslam_2111525;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class DeleteCustomerDetails
+{
+    @javafx.fxml.FXML
+    private TableColumn accountTypeTableCol;
+    @javafx.fxml.FXML
+    private TableColumn depositoryParticipantsTableCol;
+    @javafx.fxml.FXML
+    private Label messageShowingLabel;
+    @javafx.fxml.FXML
+    private TableView customerDetailsTable;
+    @javafx.fxml.FXML
+    private TableColumn bankNameTableCol;
+    @javafx.fxml.FXML
+    private TableColumn boIDTableCol;
+    @javafx.fxml.FXML
+    private TableColumn accountNameTableCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void CustomerDetailsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void deleteCustomerButtonOnAction(ActionEvent actionEvent) {
+    }
+}
