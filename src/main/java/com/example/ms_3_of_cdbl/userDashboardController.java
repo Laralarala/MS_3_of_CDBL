34dcabee1ede7aeb8befaacfa77bbb8737c2c79c
Laralarala <rafadinislamzidan@gmail.com>
2026-08-07 03:08:29 +0600

    dashboard and some littile change

--- src/main/java/com/example/ms_3_of_cdbl/userDashboardController.java
+++ src/main/java/com/example/ms_3_of_cdbl/userDashboardController.java
@@ -6,54 +6,50 @@ import javafx.scene.control.Label;
 
 public class userDashboardController
 {
-    @javafx.fxml.FXML
-    private Label messageShownLabel;
 
     @javafx.fxml.FXML
     public void initialize() {
     }
 
+
     @javafx.fxml.FXML
-    public void compilanceOfficerButtonOnAction(ActionEvent actionEvent) {
-        SceneSwitcher.switchScene(actionEvent,"/Shawrob/complianceOfficerDashboard-view.fxml","compilanceOfficer");
+    public void CustodyOfficerButton(ActionEvent actionEvent) {
+        SceneSwitcher.switchScene(actionEvent,"/SaminaToma/Dashboard_CustodyOfficerView.fxml","CustodyOfficer");
     }
 
     @javafx.fxml.FXML
-    public void merchantBankExecutiveButtonOnAction(ActionEvent actionEvent) {
-        SceneSwitcher.switchScene(actionEvent,"/SaminaToma/Dashborad_MerchantBankExecutiveView.fxml","merchantBankOfficer");
-
-    }
-
-    @javafx.fxml.FXML
-    public void depositoryParticipantsHolderButtonOnAction(ActionEvent actionEvent) {
-        SceneSwitcher.switchScene(actionEvent,"/com/example/ms_3_of_cdbl/MarsiadRahman/Dashborad_ DepositoryParticipantHolderview.fxml","depositoryParticipants");
-    }
-
-    @javafx.fxml.FXML
-    public void subscriptionManagerbUTTONoNaCTION(ActionEvent actionEvent) {
-        SceneSwitcher.switchScene(actionEvent,"/RafadinIslam_2111525/subscriptionManager.fxml","SubscriptionManager");
-
-    }
-
-    @javafx.fxml.FXML
-    public void reportOfficerButtonOnAction(ActionEvent actionEvent) {
+    public void ReportOfficerButton(ActionEvent actionEvent) {
         SceneSwitcher.switchScene(actionEvent,"/Shawrob/reportOfficerDashboard-view.fxml","reportOfficer");
-
     }
 
     @javafx.fxml.FXML
-    public void settlementOfficerButtonOnAction(ActionEvent actionEvent) {
+    public void SubscriptionManagerButton(ActionEvent actionEvent) {
+        SceneSwitcher.switchScene(actionEvent,"/RafadinIslam_2111525/subscriptionManager.fxml","SubscriptionManager");
+    }
+
+    @javafx.fxml.FXML
+    public void BoAccountManagerButton(ActionEvent actionEvent) {
+        SceneSwitcher.switchScene(actionEvent,"/RafadinIslam_2111525/boAccountManager.fxml","boaccountManager");
+    }
+
+    @javafx.fxml.FXML
+    public void MerchantBankExecutiveButton(ActionEvent actionEvent) {
+        SceneSwitcher.switchScene(actionEvent,"/SaminaToma/Dashborad_MerchantBankExecutiveView.fxml","merchantBankOfficer");
+    }
+
+    @javafx.fxml.FXML
+    public void SettlementOfficerButton(ActionEvent actionEvent) {
         SceneSwitcher.switchScene(actionEvent,"/com/example/ms_3_of_cdbl/MarsiadRahman/Dashborad_SettlementOfficerview.fxml","settlementOfficer");
     }
 
     @javafx.fxml.FXML
-    public void boAccountManagerButtonOnAction(ActionEvent actionEvent) {
-        SceneSwitcher.switchScene(actionEvent,"/RafadinIslam_2111525/boAccountManager.fxml","boaccountManager");
-
+    public void DepositoryParticipantHolderButton(ActionEvent actionEvent) {
+        SceneSwitcher.switchScene(actionEvent,"/com/example/ms_3_of_cdbl/MarsiadRahman/Dashborad_ DepositoryParticipantHolderview.fxml","depositoryParticipants");
     }
 
     @javafx.fxml.FXML
-    public void custodyOfficerButtonOnAction(ActionEvent actionEvent) {
-        SceneSwitcher.switchScene(actionEvent,"/SaminaToma/Dashboard_CustodyOfficerView.fxml","CustodyOfficer");
+    public void CompilanceOfficerButton(ActionEvent actionEvent) {
+        SceneSwitcher.switchScene(actionEvent,"/Shawrob/complianceOfficerDashboard-view.fxml","compilanceOfficer");
     }
+
 }
